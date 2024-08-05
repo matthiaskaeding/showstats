ab1b91be0f7d4c02a4d9a821ee74da25c6f37e48
matthiaskaeding <matthiaskaeding@posteo.de>
2024-08-05 14:08:44 +0200

    More focussed installations

--- noxfile.py
+++ noxfile.py
@@ -3,15 +3,20 @@ import nox
 nox.options.default_venv_backend = "uv"
 
 
+
+
 @nox.session
 def lint(session):
     session.install("ruff")
     session.run("ruff", "check")
 
-
-@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"])
+@nox.session(name="python_versions", python=["3.8", "3.9", "3.10", "3.11", "3.12"])
 def test(session):
-    session.install("-r", "dev-requirements.txt")
+    session.install("pytest>=8.3.2")
+    session.install("polars>=0.20.21")
+    session.install("pandas>=1.5.3")
+    session.install("pyarrow>=17.0.0")
+
     session.run("pytest", "tests/")
 
 
@@ -19,9 +24,10 @@ def test(session):
 @nox.parametrize("pandas_version", ["1.5.3"])
 @nox.session(name="polars_version")
 def test_polars_versions(session, polars_version, pandas_version):
-    session.install("-r", "dev-requirements.txt")
+    session.install("pytest>=8.3.2")
     session.install(f"polars=={polars_version}")
     session.install(f"pandas=={pandas_version}")
+
     session.run("pytest", "tests/")
 
 
