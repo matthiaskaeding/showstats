13e0d61ba4889b14fe4cd1485364b09af47e1214
matthiaskaeding <matthiaskaeding@posteo.de>
2024-08-07 12:29:23 +0200

    Scaling df sampling

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,8 @@
+from datetime import date, datetime
+
 import polars as pl
 import pytest
+from numpy import random as np_random
 
 
 def sample_series(
@@ -19,12 +22,9 @@ def sample_series(
     mean (float): Mean
 
     """
-    import random
+    np_random.seed(seed)
 
-    random.seed(seed)
-
-    data = [random.gauss(mu=0.0, sigma=1.0) for _ in range(n)]
-    sr = pl.Series(data)
+    sr = pl.Series(np_random.normal(size=n))
     if mean is not None:
         sr_mean = sr.mean()
         sr = (sr - sr_mean) + mean
@@ -41,6 +41,87 @@ def sample_series(
     return sr
 
 
+def sample_datetimes(
+    seed: int = 1,
+    n: int = 100,
+    start_date: date = date(1500, 1, 1),
+    end_date: date = date(2000, 1, 1),
+    name: str = "x",
+):
+    """
+    Efficiently sample a large number of datetimes within a given range using Polars.
+
+    Parameters:
+    start_date (str): The start date in 'YYYY-MM-DD' format.
+    end_date (str): The end date in 'YYYY-MM-DD' format.
+    n_samples (int): The number of samples to generate.
+    seed (int, optional): Random seed for reproducibility.
+
+    Returns:
+    pl.Series: A sorted Series of sampled datetimes.
+    """
+    import random
+
+    random.seed(seed)
+
+    # Calculate the total number of seconds in the date range
+    total_seconds = int((end_date - start_date).total_seconds())
+
+    # Generate random seconds offsets
+    random_seconds = random.choices(range(total_seconds), k=n)
+
+    return (
+        pl.DataFrame(pl.Series("random_seconds", random_seconds))
+        .select(
+            pl.lit(datetime(start_date.year, start_date.month, start_date.day)).alias(
+                name
+            )
+            + pl.duration(seconds=pl.col("random_seconds"))
+        )
+        .get_column(name)
+    )
+
+
+def sample_dates(
+    seed: int = 1,
+    n: int = 100,
+    start_date: date = date(1500, 1, 1),
+    end_date: date = date(2000, 1, 1),
+    name: str = "x",
+):
+    """
+    Efficiently sample a large number of datetimes within a given range using Polars.
+
+    Parameters:
+    start_date (str): The start date in 'YYYY-MM-DD' format.
+    end_date (str): The end date in 'YYYY-MM-DD' format.
+    n_samples (int): The number of samples to generate.
+    seed (int, optional): Random seed for reproducibility.
+
+    Returns:
+    pl.Series: A sorted Series of sampled datetimes.
+    """
+    import random
+    from datetime import date
+
+    random.seed(seed)
+
+    # Calculate the total number of seconds in the date range
+    total_seconds = int((end_date - start_date).days)
+
+    # Generate random seconds offsets
+    random_days = random.choices(range(total_seconds), k=n)
+
+    return (
+        pl.DataFrame(pl.Series("random_days", random_days))
+        .select(
+            pl.lit(date(start_date.year, start_date.month, start_date.day)).alias(name)
+            + pl.duration(days=pl.col("random_days"))
+        )
+        .get_column(name)
+    )
+
+
 def sample_df_(n: int = 100, seed: int = 1) -> pl.DataFrame:
     """
     Generate a sample DataFrame with various data types.
@@ -51,67 +132,63 @@ def sample_df_(n: int = 100, seed: int = 1) -> pl.DataFrame:
     Returns:
         pl.DataFrame: A DataFrame with sample data.
     """
-    import random
-    from datetime import date, datetime, timedelta
+    np_random.seed(seed)
 
     assert n >= 100, "There must be >= 100 rows"
 
-    random.seed(a=seed, version=2)
-    int_data = range(n)
-    float_data = [i / 100 for i in range(n)]
+    np_random.seed(seed)
     bool_data = [i % 2 == 0 for i in range(n)]
-    str_data = random.choices(["foo", "bar", "baz", "ABC"], k=n)
-    date_col = pl.date_range(
-        start=date(2022, 1, 1),
-        end=date(2022, 1, 1) + timedelta(days=n - 1),
-        eager=True,
-    )
-    date_col_2 = pl.date_range(
-        start=date(1500, 1, 1),
-        end=date(1500, 1, 1) + timedelta(days=n - 1),
-        eager=True,
-    )
-    datetime_col = pl.datetime_range(
-        start=datetime(2022, 1, 1),
-        end=datetime(2022, 1, 1) + timedelta(seconds=n - 1),
-        interval="1s",
-        eager=True,
-    )
-    datetime_col_2 = pl.datetime_range(
-        start=datetime(1995, 1, 1),
-        end=datetime(1995, 1, 1) + timedelta(seconds=n - 1),
-        interval="1s",
-        eager=True,
-    )
+    str_data = np_random.choice(["foo", "bar", "baz", "ABC"], size=n)
 
     cats = ["low", "medium", "high"]
-    categorical_data = random.choices(cats, k=n)
+    categorical_data = np_random.choice(cats, size=n)
     null_data = [None] * n
 
-    int_with_missing_data = list(int_data)
-    for i in range(10, 30):
-        int_with_missing_data[i] = None
-
-    return pl.DataFrame(
-        {
-            "int_col": int_data,
-            "int_with_missing": int_with_missing_data,
-            "float_col": float_data,
-            "float_col_with_mean_2": sample_series(n=n, seed=seed, mean=2),
-            "float_col_with_std_2": sample_series(n=n, seed=seed, std=2),
-            "float_col_with_min_7": sample_series(n=n, seed=seed, min=7),
-            "float_col_with_max_17": sample_series(n=n, seed=seed, max=17),
-            "bool_col": bool_data,
-            "str_col": str_data,
-            "date_col": date_col,
-            "date_col_2": date_col_2,
-            "datetime_col": datetime_col,
-            "datetime_col_2": datetime_col_2,
-            "categorical_col": pl.Series(categorical_data, dtype=pl.Categorical),
-            "enum_col": pl.Series(categorical_data, dtype=pl.Enum(cats)),
-            "null_col": pl.Series(null_data),
-        }
+    df = (
+        pl.LazyFrame(
+            {
+                "float_col_with_mean_2": sample_series(n=n, seed=seed, mean=2),
+                "float_col_with_std_2": sample_series(n=n, seed=seed, std=2),
+                "float_col_with_min_7": sample_series(n=n, seed=seed, min=7),
+                "float_col_with_max_17": sample_series(n=n, seed=seed, max=17),
+                "bool_col": bool_data,
+                "str_col": str_data,
+                "categorical_col": pl.Series(categorical_data, dtype=pl.Categorical),
+                "enum_col": pl.Series(categorical_data, dtype=pl.Enum(cats)),
+                "null_col": pl.Series(null_data),
+            }
+        )
+        .with_columns(
+            pl.arange(n).alias("int_col"),
+        )
+        .with_columns(
+            pl.col("int_col").truediv(100).alias("float_col"),
+            sample_dates(n=n, name="date_col", seed=1),
+            sample_dates(n=n, name="date_col_2", seed=2),
+            sample_datetimes(n=n, name="datetime_col", seed=1),
+            sample_datetimes(n=n, name="datetime_col_2", seed=2),
+        )
     )
+
+    uniforms = np_random.uniform(size=n)
+
+    # Set some values to null
+    df = df.with_columns(pl.Series(uniforms).alias("U")).with_columns(
+        pl.when(pl.col("U").lt(0.1))
+        .then(None)
+        .otherwise(pl.col("int_col"))
+        .alias("int_col_with_missings"),
+        pl.when(pl.col("U").lt(0.34))
+        .then(None)
+        .otherwise(pl.col("bool_col"))
+        .alias("bool_col"),
+        pl.when(pl.col("U").lt(0.55))
+        .then(None)
+        .otherwise(pl.col("str_col"))
+        .alias("str_col"),
+    )
+
+    return df.collect()
 
 
 @pytest.fixture(scope="session")
