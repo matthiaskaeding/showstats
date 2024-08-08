4e21719f68792065269774c34329b4742a3fe7d6
matthiaskaeding <matthiaskaeding@posteo.de>
2024-08-08 08:46:59 +0200

    First docs with conf edited

---
+++ docs/conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+from pathlib import Path
+import sys
+
+src_dir = (Path(__file__).parents[1] / "src").resolve()
+assert src_dir.exists()
+sys.path.insert(0, src_dir)
+
+project = "showstats"
+copyright = "2024, Matthias Kaeding"
+author = "Matthias Kaeding"
+release = "0.0.2"
+
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
